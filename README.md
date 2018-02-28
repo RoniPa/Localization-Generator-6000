3b33973bc2fde832b3333837189f4f25c9e582b5
Roni P <roni.palvaaho@gmail.com>
2018-03-01 01:43:46 +0200

    Added append functionality

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Localization Generator 6000
-Simple python tool for generating (empty) localization json files for [Ngx-Translate](https://github.com/ngx-translate/core) from source code.
+Simple python tool for generating localization json files for [Ngx-Translate](https://github.com/ngx-translate/core) from source code.
 Uses RegExp match for finding translate pipe in templates and parses tags from them.
 
 _Note: Doesn't support all formats of localization, only parses from templates!_
@@ -17,6 +17,22 @@ Run with command ```locgen```
 * __--p:__ Source path (e.g. "./src/")
 * __--o:__ Output file (e.g. "./localizations/en-US.json")
 * __--e:__ File encoding (e.g. "utf-8"). Defaults to utf-8.
+
+### Options:
+__-p, --path__
+Source path (e.g. "./src/")
+
+__-o, --output__
+Output file (e.g. "./localizations/en-US.json")
+
+__-e, --encoding__
+File encoding (e.g. "utf-8")
+
+__-a, --append / -na, --no-append__
+Append content - if file exists, existing translations remain. Removed or moved tags are still destroyed. Enabled by default.
+
+__--help__
+Show this message and exit.
 
 If source & output are not given with args, they're prompted for.
 
--- locgen.py
+++ locgen.py
@@ -5,48 +5,73 @@ import json
 import click
 
 @click.command()
-@click.option('--p', help='Source path (e.g. "./src/")')
-@click.option('--o', help='Output file (e.g. "./localizations/en-US.json")')
-@click.option('--e', default='utf-8', help='File encoding (e.g. "utf-8")')
-def recurse_files(p, o, e):
+@click.option('-p', '--path', help='Source path (e.g. "./src/")')
+@click.option('-o', '--output', help='Output file (e.g. "./localizations/en-US.json")')
+@click.option('-e', '--encoding', default='utf-8', help='File encoding. Defaults to UTF-8.', type=click.Choice(['utf-8', 'latin-1', 'ascii']))
+@click.option('-a/-na', '--append/--no-append', default=True, help='Append content - if file exists, existing translations remain. Removed or moved tags are still destroyed. Enabled by default.')
+def recurse_files(path, output, encoding, append):
     
-    if (p == None):
-        p = click.prompt('Give source root: (e.g. "./src/")', type=str)
+    if (path == None):
+        path = click.prompt('Give source root: (e.g. "./src/")', type=str)
 
-    if (o == None):
-        o = click.prompt('Give output file: (e.g. "./localizations/en-US.json")', type=str)
+    if (output == None):
+        output = click.prompt('Give output file: (e.g. "./localizations/en-US.json")', type=str)
 
     pattern = '(\'|\")\s?\|\s?translate'
     dict_map = {}
+    ref_map = {}
+    
+    # If file exists, read in as reference
+    if (append and os.path.isfile(output)): 
+        try:
+            with open(output, 'r', encoding=encoding) as json_data:
+                ref_map = json.load(json_data)
+        except:
+            click.echo(click.style("Unexpected error: %s" % sys.exc_info()[0], fg="red"))
+            raise click.Abort()
 
-    for root, directories, filenames in os.walk(p):
+    for root, directories, filenames in os.walk(path):
         gen = (x for x in filenames if x.endswith('.html') or x.endswith('.ts') or x.endswith('.js'))
         for filename in gen:
             try:
-                with open(os.path.join(root, filename), encoding=e) as f:
+                with open(os.path.join(root, filename), encoding=encoding) as f:
                     for line in f:
                         for m in re.finditer(pattern, line):
                             end = m.start(0)
                             start = line[:end].rfind(line[end])
                             tags = (line[start+1:end]).split('.')
 
-                            d = dict_map
+                            d1 = dict_map
+                            d2 = ref_map
                             last_i = len(tags)-1
+
                             for i in range(0, last_i): 
-                                if tags[i] not in d:
-                                    d[tags[i]] = {}
-                                d = d[tags[i]]
-                            d[tags[last_i]] = ""
+                                if tags[i] not in d1:
+                                    d1[tags[i]] = {}
+
+                                if tags[i] in d2:
+                                    d2 = d2[tags[i]]
+
+                                d1 = d1[tags[i]]
+                            
+                            if append and tags[last_i] in d2 and type(d2[tags[last_i]]) is str:
+                                d1[tags[last_i]] = d2[tags[last_i]]  
+                            else:
+                                d1[tags[last_i]] = ""
+                            
+            except TypeError:
+                click.echo(click.style("TypeError... Is a tag overwritten?", fg="red"))
+                raise click.Abort()
             except:
                 click.echo(click.style("Unexpected error: %s" % sys.exc_info()[0], fg="red"))
                 raise click.Abort()
                 
     try:
-        with open(o, 'w', encoding=e) as f:
+        with open(output, 'w', encoding=encoding) as f:
             f.write(json.dumps(dict_map, sort_keys=True, indent=4))
     except:
         click.echo(click.style("Unexpected error: %s" % sys.exc_info()[0], fg="red"))
         raise click.Abort()
 
-    click.echo("File generated at %s" % o)
+    click.echo("File generated at %s" % output)
     click.echo("Have a nice day! :)")
