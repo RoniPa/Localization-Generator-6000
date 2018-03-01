1bdf664adfeb898bfb10b59fe6c4685b613b3de7
Roni P <roni.palvaaho@gmail.com>
2018-03-01 15:54:24 +0200

    Correctly handle utf8 encoding

--- locgen.py
+++ locgen.py
@@ -4,10 +4,21 @@ import re
 import json
 import click
 
+def byteify(input):
+    if isinstance(input, dict):
+        return {byteify(key): byteify(value)
+                for key, value in input.items()}
+    elif isinstance(input, list):
+        return [byteify(element) for element in input]
+    elif isinstance(input, str):
+        return input.encode('utf-8')
+    else:
+        return input
+
 @click.command()
 @click.option('-p', '--path', help='Source path (e.g. "./src/")')
 @click.option('-o', '--output', help='Output file (e.g. "./localizations/en-US.json")')
-@click.option('-e', '--encoding', default='utf-8', help='File encoding. Defaults to UTF-8.', type=click.Choice(['utf-8', 'latin-1', 'ascii']))
+@click.option('-e', '--encoding', default='utf8', help='File encoding. Defaults to UTF-8.', type=click.Choice(['utf8', 'latin-1', 'ascii']))
 @click.option('-a/-na', '--append/--no-append', default=True, help='Append content - if file exists, existing translations remain. Removed or moved tags are still destroyed. Enabled by default.')
 def recurse_files(path, output, encoding, append):
     
@@ -67,8 +78,10 @@ def recurse_files(path, output, encoding, append):
                 raise click.Abort()
                 
     try:
-        with open(output, 'w', encoding=encoding) as f:
-            f.write(json.dumps(dict_map, sort_keys=True, indent=4))
+        with open(output, 'wb') as f:
+            s = json.dumps(dict_map, sort_keys=True, indent=4, ensure_ascii=False).encode(encoding)
+            f.write(s)
+
     except:
         click.echo(click.style("Unexpected error: %s" % sys.exc_info()[0], fg="red"))
         raise click.Abort()
